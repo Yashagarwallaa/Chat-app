21928aebaee231ae267cc30f9ff31028cb98a8cd
Yashagarwallaa <yashagarwalla001@gmail.com>
2023-08-12 13:57:09 +0530

    Realtime-chat app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { getDatabase,push,ref,set,onChildAdded, update } from "firebase/database";
+import { GoogleAuthProvider,getAuth, signInWithPopup } from "firebase/auth";
 import './App.css';
 
-function App() {
+// Add log-out button // Add styling
+
+function App(){
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+  const googleLogin = ()=>{
+   
+    signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      setUser({name: result.user.displayName ,email: result.user.email});
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    })
+  }
+
+  const [user,setUser] = useState('');
+  const [chats,setchats] = useState([]); 
+  const [msg,setMsg] = useState('')
+  const db = getDatabase();
+  const chatListRef = ref(db, 'chats');
+  
+
+  const updateHeight=()=>{
+    const el = document.getElementById('chat');
+    if (el){
+      el.scrollTop = el.scrollHeight;
+    }
+  }
+
+  useEffect( ()=>{
+    
+    onChildAdded(chatListRef, (data) => {
+      setchats(chats=>[...chats,data.val()]);
+      setTimeout( ()=>{
+        updateHeight();
+      },100)
+      
+    });
+  },[])
+
+
+
+ const sendChat = ()=>{
+
+ 
+ const chatRef = push(chatListRef);
+ set(chatRef, {
+       user , message : msg 
+ });
+
+  // const c = [...chats];
+  // c.push({name,message:msg});
+  // setchats(c);
+
+  setMsg('')
+ };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      {user.email? null: <div> 
+        {/* <input type='text' placeholder='Enter your user' 
+      onBlur={e=>setUser(e.target.value)}>
+        </input> */}
+        <button onClick={e=>{googleLogin()}}>Google Sign in</button>
+        </div>
+      }
+      {user.email?<div>
+       <h1>User: {user.name}</h1>
+       <div id = "chat"class = "chat-container">
+        {chats.map( c=>
+        <div class ={`container ${c.user.email === user.email  ? 'me': '' }`}>
+        < p class = "chatbox">
+          <strong>{c.user.name} :  </strong>
+          <span>{c.message}</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        </div>)}
+       
+       </div>
+       </div> :null}
+       <div class="btn">
+        <input type="text" onInput={e=>setMsg(e.target.value)} value={msg} placeholder="Enter text"></input>
+        <button onClick={e=>sendChat()}>Send</button>
+       </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBvJ4TopsDSWX-D6EKN5gUNfrDJNZHXCCs",
+  authDomain: "chat-app-f276f.firebaseapp.com",
+  databaseURL: "https://chat-app-f276f-default-rtdb.firebaseio.com",
+  projectId: "chat-app-f276f",
+  storageBucket: "chat-app-f276f.appspot.com",
+  messagingSenderId: "405440143964",
+  appId: "1:405440143964:web:6116ec9f732050ebbd7c99"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
